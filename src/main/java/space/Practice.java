26e17075ab4ceb2d40c5d6bbc49abf9380c48398
qqyang <2605578650@qq.com>
2021-01-12 22:42:44 +0800

    test

--- src/main/java/space/Practice.java
+++ src/main/java/space/Practice.java
@@ -1,13 +1,34 @@
 package space;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Practice {
     public static void main(String[] args) {
-        int[] arr = new int[5];
+//        test1();
+        test2();
+    }
+    public static void test1(){
+        String[] arr = new String[5];
+        int[] b = new int[5];
         for (int i = 0; i < arr.length; i++){
-            arr[i] = i;
+            String a = "aaa";
+            arr[i] = a;
+            b[i] = i;
         }
         System.out.println(Arrays.toString(arr));
+        List<String> strings = Arrays.asList(arr);
+        List<int[]> ints = Arrays.asList(b);
+        System.out.println(strings);
+        int[] ints1 = ints.get(0);
+        for (int i = 0; i <ints1.length; i++){
+            System.out.println(ints1[i]);
+        }
+    }
+    static void test2(){
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add(null);
+        strings.add(null);
+        System.out.println(strings.size());
     }
 }
