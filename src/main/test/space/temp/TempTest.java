1db2bd104570c787f79b8f15b6ca283ac98a3e09
qqyang <2605578650@qq.com>
2021-01-24 21:58:09 +0800

    test

--- src/main/test/space/temp/TempTest.java
+++ src/main/test/space/temp/TempTest.java
@@ -1,15 +1,17 @@
 package space.temp;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+
+@Slf4j
 public class TempTest {
     @Test
-    public void test(){
-        String[] strings = new String[5];
-        for (int i = 0; i < strings.length; i++){
-            strings[i] = "" + i;
-            System.out.println(strings[i]);
-        }
-
+    public void test(HttpServletRequest request) throws JsonProcessingException, InterruptedException {
+        System.out.println("test.........");
+        request.getCookies();
     }
 }
