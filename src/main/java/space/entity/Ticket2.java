20f1929169e3ce04ab294c4383a981446a2d3a6e
qqyang <2605578650@qq.com>
2021-03-23 22:06:42 +0800

    多线程测试

---
+++ src/main/java/space/entity/Ticket2.java
@@ -0,0 +1,13 @@
+package space.entity;
+
+public class Ticket2 implements Runnable{
+    private int tick = 100;
+    @Override
+    public void run() {
+        while (true){
+            if (tick > 0){
+                System.out.println(Thread.currentThread().getName() + " tick:" + --tick);
+            }
+        }
+    }
+}
