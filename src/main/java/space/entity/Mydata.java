4a9697b3ad333b3d8e5cc439755dd9c0a08bed6d
qqyang <2605578650@qq.com>
2021-03-27 22:54:29 +0800

    Volatile原子性测试

--- src/main/java/space/entity/Mydata.java
+++ src/main/java/space/entity/Mydata.java
@@ -1,10 +1,16 @@
 package space.entity;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Mydata {
 //        volatile public int number = 0;
     public int number = 0;
+//    AtomicInteger a = new AtomicInteger();
 
     public void addTo60() {
         this.number = 60;
+    }
+    public void addOne(){
+        this.number++;
     }
 }
--- src/main/java/space/juc/Volatile.java
+++ src/main/java/space/juc/Volatile.java
@@ -1,15 +1,37 @@
 package space.juc;
 
+import lombok.SneakyThrows;
 import space.entity.Mydata;
 
 /**
- * @Description 验证volatile的可见性
+ * @Description 验证volatile的可见性、（是否保证）原子性
  * @Author space
  * @Date 2021/3/26
  */
 
 public class Volatile {
+    @SneakyThrows
     public static void main(String[] args) {
+//        testVisibility();
+        testAtomic();//volatile不保证原子性
+    }
+
+    private static void testAtomic() throws InterruptedException {
+        Mydata mydata = new Mydata();
+        for (int i = 0; i < 20; i++){
+            new Thread(() -> {
+                for (int j = 0; j < 1000; j++)
+                    mydata.addOne();
+            }, String.valueOf(i)).start();
+        }
+        Thread.sleep(11000);
+//        while(Thread.activeCount() > 3){
+//            Thread.yield();
+//        }
+        System.out.println(mydata.number);
+    }
+
+    private static void testVisibility() {
         Mydata mydata = new Mydata();
         new Thread(() -> {
             System.out.println(Thread.currentThread().getName() + "\t come in");
@@ -27,7 +49,8 @@ public class Volatile {
 //            System.out.println(Thread.activeCount());
 //            System.out.println("test");//todo 这里为什么加上任意语句就能over了，说明不加volatile也能刷新主内存 参考周阳04节
 //            System.out.println(Thread.currentThread().getName() + ": "+mydata.number);
-        };
+        }
+        ;
 
         System.out.println(Thread.currentThread().getName() + "\t over");
     }
