4df91255fe6e5da62e0bf1c79e160bf6c058a7ae
qqyang <2605578650@qq.com>
2021-07-08 22:02:30 +0800

    refine code

--- springboot-websocket/src/main/java/com/space/config/WebSocketServer.java
+++ springboot-websocket/src/main/java/com/space/config/WebSocketServer.java
@@ -105,7 +105,7 @@ public class WebSocketServer {
      * 实现服务器主动推送
      */
     public void sendMessage(String message) throws IOException {
-        this.session.getBasicRemote().sendText(message);
+        this.session.getBasicRemote().sendText(message);//推送给客户端，而不是服务端
     }
 
     /**
--- springboot-websocket/src/main/java/com/space/controller/TestController.java
+++ springboot-websocket/src/main/java/com/space/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.space.controller;
 
 import com.space.config.WebSocketServer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import java.io.IOException;
 
+@Slf4j
 @RestController
 public class TestController {
     @GetMapping("index")
@@ -21,8 +23,10 @@ public class TestController {
         return new ModelAndView("websocket");
     }
 
-    @RequestMapping("/push/{toUserId}")
-    public ResponseEntity<String> pushToWeb(String message, @PathVariable String toUserId) throws IOException {
+    @RequestMapping("/push/{toUserId}/{message}")
+    public ResponseEntity<String> pushToWeb(@PathVariable("message") String message,
+                                            @PathVariable("toUserId") String toUserId) throws IOException {
+        log.debug("toUserId:{}", toUserId);
         WebSocketServer.sendInfo(message, toUserId);
         return ResponseEntity.ok("MSG SEND SUCCESS");
     }
